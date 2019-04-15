4ddee362d3fb9fb258d247d9c1b2af1a976cf7e5
Mikolaj <43639758+snoh666@users.noreply.github.com>
2019-04-15 13:32:26 +0200

    Added comments to code

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 window.addEventListener('load', () => {
 
+  //Get sounds element
   const sounds = document.getElementsByClassName('sounds');
+  //Get pads elements
   const pads = document.querySelectorAll('.pads div');
+  //Get visual box
   const visual = document.getElementsByClassName('visuals')[0];
+  //Set colors for all the pads to use in visual
   const colors = [
     "#60d394",
     "#d36060",
@@ -12,21 +16,32 @@ window.addEventListener('load', () => {
     "#60c2d3"
   ];
 
+  //Add listener and functionality to every pad
   pads.forEach((pad, index) => {
     pad.addEventListener('click', function() {
+      //Set current time if user clicks again while it was already playing
       sounds[index].currentTime = 0;
+      //Play sound
       sounds[index].play();
+      //Start visual bubble function
       createBubble(index);
     });
   });
 
   const createBubble = index => {
-
+  
+    //Create div bubble element
     const bubble = document.createElement('div');
+    //Append bubble into visual box element
     visual.appendChild(bubble);
+    //Set backgound color for correct pad clicked
     bubble.style.backgroundColor = colors[index];
+    //Add animation to bubble for jump
     bubble.style.animation = `jump 1s ease`;
+    //Add listener for bubble after animation ends
     bubble.addEventListener('animationend', function() {
+      //Remove bubble after transition ends
+      //No need to use it again
       visual.removeChild(this);
     });
 
